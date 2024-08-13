19717f088d8520d01915e161f1f63714bd7666e0
cameronpool <cameronpool2019@gmail.com>
2024-08-13 18:25:17 -0500

    about section finished

--- Professional Portfolio/src/components/About/About.jsx
+++ Professional Portfolio/src/components/About/About.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from './About.module.css';
 
 export const About = () => {
-    return <div>About</div>
+  return (
+    <section className={styles.container} id='about'>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.jpg")}
+          alt="me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutIcons} src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>front end with experience responsive blah blah</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutIcons} src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>back end with experience responsive blah blah backend also APIS?</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutIcons} src={getImageUrl("about/brainIcon.png")} alt="Brain icon" />
+            <div className={styles.aboutItemText}>
+              <h3>AI Engineer</h3>
+              <p>I have experience and many projects utilizing Machine Learning, Deep Learning, LLM, and NLP </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
