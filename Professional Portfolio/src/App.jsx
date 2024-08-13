f6acd1bbca42793bd01cc63fff5ad0082750b87a
cameronpool <cameronpool2019@gmail.com>
2024-08-13 15:21:50 -0500

    colors and fonts set up (using modules for css)

--- Professional Portfolio/src/App.jsx
+++ Professional Portfolio/src/App.jsx
@@ -1,13 +1,7 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      Hello World!
-    </>
-  )
+  return <h1>Hello World!</h1>
 }
 
 export default App
