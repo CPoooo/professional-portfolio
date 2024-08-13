b7850e25c748c720bac4f8f3578f6744497df47f
cameronpool <cameronpool2019@gmail.com>
2024-08-13 15:37:02 -0500

    utils.js for images urls created

--- Professional Portfolio/src/App.jsx
+++ Professional Portfolio/src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
-  return <h1>Hello World!</h1>
+  return <div className={styles.App}>Hello World!</div>
 }
 
 export default App
