872ca8f02e7bca0b87aad19e423f5928670e26dc
cameronpool <cameronpool2019@gmail.com>
2024-08-14 17:11:17 -0500

    demos under construction

--- Professional Portfolio/src/components/Projects/Projects.jsx
+++ Professional Portfolio/src/components/Projects/Projects.jsx
@@ -6,7 +6,13 @@ import styles from "./Projects.module.css";
 export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
-      <h2 className={styles.title}>Projects</h2>
+      <h2 className={styles.title}>
+        Projects{" "}
+        <span>
+          (Demos are in the process of being deployed but all the source code is
+          still available)
+        </span>
+      </h2>
       <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
