4a6a42aaf1cc9d9a1de552d0066e65d0f896fcab
cameronpool <cameronpool2019@gmail.com>
2024-08-14 12:52:16 -0500

    Experience and skills section complete

--- Professional Portfolio/src/App.jsx
+++ Professional Portfolio/src/App.jsx
@@ -2,12 +2,14 @@ import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
 import { Hero } from './components/Hero/Hero'
 import { About } from './components/About/About'
+import { Experience } from './components/Experience/Experience'
 
 function App() {
   return <div className={styles.App}>
     <Navbar />
     <Hero />
     <About />
+    <Experience />
   </div>
 }
 
